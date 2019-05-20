083287b187841bcbaf407a1b1d981db783043052
Unknown <sylvainlasarte@gmail.com>
2019-05-20 14:28:21 +0200

    300marker

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,10 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Map, latLng, tileLayer, Layer, marker, icon } from 'leaflet';
+import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation'; 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ReadKeyExpr } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-tab2',
@@ -10,8 +15,17 @@ export class Tab2Page {
   map: Map;
   lat: number = 45.188096;
   lng: number =  5.718452;
+  options : GeolocationOptions;
+  currentPos : Geoposition;
+  items : any;
 
-  ionViewDidEnter() { this.loadMap(); }
+  ionViewDidEnter() {
+    //this.getUserPosition();
+    this.loadMap();
+  }
+
+  constructor() {
+  }
 
   loadMap() {
     setTimeout(() => {
@@ -31,8 +45,63 @@ export class Tab2Page {
       }).addTo(this.map)
       .bindPopup('Salut <br> ça va?');
 
+      fetch('https://data.metromobilite.fr/api/bbox/json')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data['features'])
+        console.log(data['features']['0']['geometry']['coordinates']['0']);
+        let id = 0;
+        data['features'].forEach((value) => {
+          marker([value['geometry']['coordinates']['1'],value['geometry']['coordinates']['0']], {
+            icon: icon({
+              iconSize: [ 25, 41 ],
+              iconAnchor: [ 13, 41 ],
+              iconUrl: 'leaflet/marker-icon.png',
+              shadowUrl: 'leaflet/marker-shadow.png'
+            })
+          }).addTo(this.map)
+          .bindPopup('Salut <br> ça va?');
+          id += 1;
+          let BreakException = {};
+          if(id == 300) throw BreakException;
+        });
+      })
+      .catch(err => {
+        // Do something for an error here
+      })
+
+      /*marker([,], {
+        icon: icon({
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: 'leaflet/marker-icon.png',
+          shadowUrl: 'leaflet/marker-shadow.png'
+        })
+      }).addTo(this.map)
+      .bindPopup('Salut <br> ça va?');*/
+
     }, 50);
+      
+    };
+
+    getUserPosition(){
+      this.options = {
+          enableHighAccuracy : true
+      };
+  
+      /*this.geolocation.getCurrentPosition().then((pos : Geoposition) => {
+  
+          this.currentPos = pos;      
+          console.log(pos);
+  
+      },(err : PositionError)=>{
+          console.log("error : " + err.message);
+      });*/
   }
+
+
 
   /** Remove map when we have multiple map object */
   ionViewWillLeave() {
